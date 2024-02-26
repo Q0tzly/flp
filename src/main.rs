d53bad54b1eb5b2c6ac7c9421c3c745e52eebf07
Q0tzly <g9164902@gmail.com>
2024-02-27 00:08:30 +0900

    update: use type option to sent path to fn that get full path

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::env;
+//use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let len = args.len();
 
     println!("{}", len);
+
+    let path: Option<&str> = match len {
+        1 => Some("."),
+        2 => Some(args[1].as_str()),
+        _ => None,
+    };
+
+    println!("{:?}", path);
 }
