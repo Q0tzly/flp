c17d5c55540647cd72890fd3653a6fcc6549f5db
Q0tzly <g9164902@gmail.com>
2024-02-27 18:14:29 +0900

    update: organized code and useful help.

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,36 @@ use std::env;
 use std::path::PathBuf;
 use std::process;
 
-const RED: &str = "\x1b[31m";
-const GREEN: &str = "\x1b[32m";
-const CYAN: &str = "\x1b[36m";
-const RESET: &str = "\x1b[0m";
+const NO_SUCH: &str = "\x1b[31m\x1b[1mHelp: \x1b[0m\x1b[0mNo such file or directory.";
+const MANY_ARGS: &str = "\x1b[31m\x1b[1mHelp: \x1b[0m\x1b[0mToo many arguments provided.";
 
-const NAME: &str = "\x1b[0mGet Full Path Command\n\n";
+const TO_HELP: &str =
+    "\n\x1b[1m\x1b[32mUsage:\x1b[0m\n  \x1b[36mflp -h, --help     \x1b[0mput help\x1b[0m";
 
-fn to_help() -> String {
-    format!(
-        "{}{}help:\n  {}flp -h    {}put help",
-        NAME, GREEN, CYAN, RESET
-    )
-}
+const VERSION: &str = "flp    0.1.0 (2024-02-27)";
 
-fn help() {
-    println!("{}{}Usage:\n  {}flp -h        {}put help\n  {}flp           {}put working dir\n  {}flp <PATH>    {}put full path of file or dir",
-        NAME, GREEN, CYAN, RESET, CYAN, RESET, CYAN, RESET
-    );
-}
+const HELP: &str =
+    "\x1b[0mGet Full Path Command\n\n\x1b[1m\x1b[32mUsage:\x1b[0m\n  \x1b[36mflp -h, --help     \x1b[0mput help\n  \x1b[36mflp -v, --version  \x1b[0mput version\n  \x1b[36mflp                \x1b[0mput working dir\n  \x1b[36mflp <PATH>         \x1b[0mput full path of file or dir";
 
-fn or_help(path: &str) {
-    if path == "-h" || path == "--help" {
-        help();
+fn or_options(path: &str) {
+    let res: Option<&str> = match path {
+        "-h" | "--help" => Some(HELP),
+        "-v" | "--version" => Some(VERSION),
+        _ => None,
+    };
+
+    if let Some(res) = res {
+        println!("{}", res);
         process::exit(0);
     }
 }
 
 fn get_fpath(path: &str) -> String {
-    or_help(path);
     let path = PathBuf::from(path);
     match path.canonicalize() {
         Ok(fpath) => fpath.to_string_lossy().to_string(),
         Err(_) => {
-            println!("{}Not found that file or dir\n\n{}", RED, to_help());
+            println!("{}{}", NO_SUCH, TO_HELP);
             process::exit(0);
         }
     }
@@ -52,8 +48,11 @@ fn main() {
     };
 
     let fpath: String = match path {
-        Some(path) => get_fpath(path),
-        None => to_help(),
+        Some(path) => {
+            or_options(path);
+            get_fpath(path)
+        }
+        None => format!("{}{}", MANY_ARGS, TO_HELP),
     };
 
     println!("{}", fpath);
